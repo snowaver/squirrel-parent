19876c7f388da03e28f8d31b2d02fef4230ce232
snowaver <1123009419@qq.com>
2019-06-23 23:11:00 +0800

    1/add placeholders for close call reasons and add columns  (is_local and local_description) for chat message and group chat message table.

--- squirrel-paip/src/main/java/cc/mashroom/squirrel/paip/message/call/CloseCallReason.java
+++ squirrel-paip/src/main/java/cc/mashroom/squirrel/paip/message/call/CloseCallReason.java
@@ -21,10 +21,12 @@ import  lombok.Getter;
 @AllArgsConstructor
 public  enum  CloseCallReason
 {
-	UNKNOWN(0x00),ROOM_NOT_FOUND(0x01),STATE_ERROR(0x02),CANCEL(0x03),TIMEOUT(0x04),REJECT(0x05),BY_USER(0x06),NETWORK_ERROR( 0x07 );
+	UNKNOWN(0x00,null),ROOM_NOT_FOUND(0x01,"$(0e01)"),STATE_ERROR(0x02,"$(0e02)"),CANCEL(0x03,"$(0e03)"),TIMEOUT(0x04,"$(0e04)"),REJECT(0x05,"$(0e05)"),BY_USER(0x06,"$(0e06)"),NETWORK_ERROR( 0x07,"$(0e07)" );
 	
 	@Getter
 	private  int  value;
+	@Getter
+	private  String  placeholder;
 	
 	public  static  CloseCallReason  valueOf( int  value )
 	{
