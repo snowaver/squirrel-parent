415c1c237204c9d7bb2f8b8d9724f317e2fb807b
snowaver <1123009419@qq.com>
2019-08-13 23:07:46 +0800

    1/resolve packet type error in class newsprofile.

--- squirrel-client/src/main/java/cc/mashroom/squirrel/client/storage/model/chat/NewsProfile.java
+++ squirrel-client/src/main/java/cc/mashroom/squirrel/client/storage/model/chat/NewsProfile.java
@@ -39,7 +39,7 @@ public  class     NewsProfile
 	private  Timestamp   createTime;
 	@JsonProperty( value="PACKET_TYPE" )
 	@Column( name="PACKET_TYPE" )
-	private  Integer  packetType;
+	private  Short    packetType;
 	@JsonProperty( value="CONTACT_ID"  )
 	@Column( name="CONTACT_ID"  )
 	private  Long  contactId;
